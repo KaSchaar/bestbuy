5cb40b45b6b3d914097338f1c1fed6d362ad5d50
Katy Lübben <lady.advocat@hotmail.de>
2025-06-27 17:35:47 +0200

    Startprojekt mit Produktklasse

---
+++ products.py
@@ -0,0 +1,62 @@
+class Produkt:
+    def __init__(self, name, preis, menge):
+        if name == "":
+            raise Exception("Name darf nicht leer sein")
+        if preis < 0:
+            raise Exception("Preis darf nicht negativ sein")
+        if menge < 0:
+            raise Exception("Menge darf nicht negativ sein")
+
+        self.name = name
+        self.preis = preis
+        self.menge = menge
+        self.aktiv = True
+
+    def gib_menge(self):
+        return self.menge
+
+    def setze_menge(self, menge):
+        if menge < 0:
+            raise Exception("Ungültige Menge")
+        self.menge = menge
+        if menge == 0:
+            self.deaktiviere()
+
+    def ist_aktiv(self):
+        return self.aktiv
+
+    def aktiviere(self):
+        self.aktiv = True
+
+    def deaktiviere(self):
+        self.aktiv = False
+
+    def anzeigen(self):
+        return f"{self.name}, Preis: {self.preis}, Menge: {self.menge}"
+
+    def kaufen(self, menge):
+        if not self.aktiv:
+            raise Exception("Produkt nicht aktiv")
+        if menge <= 0:
+            raise Exception("Menge muss positiv sein")
+        if menge > self.menge:
+            raise Exception("Nicht genug auf Lager")
+        self.menge -= menge
+        if self.menge == 0:
+            self.deaktiviere()
+        return menge * self.preis
+
+
+if __name__ == "__main__":
+    bose = Produkt("Bose QuietComfort Earbuds", 250, 500)
+    mac = Produkt("MacBook Air M2", 1450, 100)
+
+    print(bose.kaufen(50))
+    print(mac.kaufen(100))
+    print(mac.ist_aktiv())
+
+    print(bose.anzeigen())
+    print(mac.anzeigen())
+
+    bose.setze_menge(1000)
+    print(bose.anzeigen())
